d984f3a0fe8f4edb3048f1e0e9aba7aafd51d8b1
abdallahshaban <abdallahshaban200@gmail.com>
2024-06-18 21:10:23 +0300

    video 37 : 1- testData part two .. test controller

--- lib/controllers/test_controller.dart
+++ lib/controllers/test_controller.dart
@@ -1,6 +1,31 @@
+import 'package:e_commerce_app14/core/class/status_request.dart';
+import 'package:e_commerce_app14/core/functions/handling_data_controller.dart';
 import 'package:e_commerce_app14/data/dataSource/remote/test_data.dart';
 import 'package:get/get.dart';
 
-class TestConteoller extends GetxController {
+class TestDataConteoller extends GetxController {
+
+
   TestData testData =TestData(Get.find());
+  List data = [];
+
+  late StatusRequest statusRequest;
+
+  getData2 ()async{
+    statusRequest = StatusRequest.loading;
+    var response = await testData.getData();
+
+    statusRequest = handlingData(response);
+
+    if (StatusRequest.success == statusRequest) {
+      data.addAll(response);
+    }
+    update();
+
+  }
+  @override
+  void onInit() {
+    getData2();
+    super.onInit();
+  }
 }
